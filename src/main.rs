4e814089238e420b8ea49eaae88e143a4287a9cc
Lever <wangzheng@shimo.im>
2019-06-08 15:56:34 +0800

    feat: loog change light

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::thread::sleep;
 use core::borrow::Borrow;
+use std::time::Duration;
 
 // 把红绿灯看成一个状态机，状态转换过程如下：
 //+-----------+      +------------+      +---------+
@@ -20,11 +21,20 @@ struct TrafficLight {
     state: TrafficLightState,
 }
 
-fn change_light(state: &TrafficLightState) {
+fn change_light(mut state: &TrafficLightState) -> TrafficLightState {
     match state {
-        TrafficLightState::Green { waiting_time } => sleep(*waiting_time),
-        TrafficLightState::Red { waiting_time } => sleep(*waiting_time),
-        TrafficLightState::Yellow { waiting_time } => sleep(*waiting_time)
+        TrafficLightState::Green { waiting_time } => {
+            sleep(*waiting_time);
+            TrafficLightState::Yellow { waiting_time: std::time::Duration::new(10, 0) }
+        },
+        TrafficLightState::Red { waiting_time } => {
+            sleep(*waiting_time);
+            TrafficLightState::Green { waiting_time: std::time::Duration::new(60, 0) }
+        },
+        TrafficLightState::Yellow { waiting_time } => {
+            sleep(*waiting_time);
+            TrafficLightState::Red { waiting_time: std::time::Duration::new(60, 0) }
+        }
     }
 }
 
@@ -32,15 +42,9 @@ fn main() {
     let mut state_machine = TrafficLight{
         state: TrafficLightState::Green { waiting_time: std::time::Duration::new(60, 0) }
     };
-    println!("{:?}", state_machine.state);
-    change_light(&state_machine.state);
-    state_machine.state = TrafficLightState::Yellow { waiting_time: std::time::Duration::new(10, 0) };
-    println!("{:?}", state_machine.state);
-    change_light(&state_machine.state);
-    state_machine.state = TrafficLightState::Red { waiting_time: std::time::Duration::new(60, 0) };
-    println!("{:?}", state_machine.state);
-    change_light(&state_machine.state);
-    state_machine.state = TrafficLightState::Green { waiting_time: std::time::Duration::new(60, 0) };
-    println!("{:?}", state_machine.state);
-    change_light(&state_machine.state);
+
+    loop {
+        println!("{:?}", state_machine.state);
+        state_machine.state = change_light(&state_machine.state)
+    }
 }
