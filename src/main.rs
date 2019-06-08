87e371de5ea615b614af1c17aa56d596d348fe8c
Lever <wangzheng@shimo.im>
2019-06-08 22:54:07 +0800

    perf: use type conversion for state transition

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::thread::sleep;
 use core::borrow::Borrow;
 use std::time::Duration;
+use std::cmp::Ordering::Greater;
 
 // 把红绿灯看成一个状态机，状态转换过程如下：
 //+-----------+      +------------+      +---------+
@@ -11,46 +12,116 @@ use std::time::Duration;
 //      +-------------------------------------+
 
 #[derive(Debug)]
-enum TrafficLightState {
-    Red { waiting_time: std::time::Duration },
-    Yellow { waiting_time: std::time::Duration },
-    Green { waiting_time: std::time::Duration },
+struct Red {
+    wait_time: Duration
 }
 
-struct TrafficLight {
-    state: TrafficLightState,
-}
-
-impl TrafficLight {
+impl Red {
     fn new() -> Self {
-        TrafficLight {
-            state: TrafficLightState::Green { waiting_time: std::time::Duration::new(60, 0) }
+        Red{
+            wait_time: Duration::new(60, 0)
         }
     }
+}
 
-    fn change_light(&mut self) {
-        self.state = match self.state {
-            TrafficLightState::Green { waiting_time } => {
-                sleep(waiting_time);
-                TrafficLightState::Yellow { waiting_time: std::time::Duration::new(10, 0) }
-            },
-            TrafficLightState::Red { waiting_time } => {
-                sleep(waiting_time);
-                TrafficLightState::Green { waiting_time: std::time::Duration::new(60, 0) }
-            },
-            TrafficLightState::Yellow { waiting_time } => {
-                sleep(waiting_time);
-                TrafficLightState::Red { waiting_time: std::time::Duration::new(60, 0) }
-            }
+#[derive(Debug)]
+struct Green {
+    wait_time: std::time::Duration
+}
+
+impl Green {
+    fn new() -> Self {
+        Green{
+            wait_time: Duration::new(60, 0)
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Yellow {
+    wait_time: std::time::Duration
+}
+
+impl Yellow {
+    fn new() -> Self {
+        Yellow{
+            wait_time: Duration::new(10, 0)
+        }
+    }
+}
+
+#[derive(Debug)]
+struct TrafficLight<TLS> {
+    state: TLS,
+}
+
+impl TrafficLight<Green> {
+    fn new() -> Self {
+        TrafficLight {
+            state: Green::new(),
+        }
+    }
+}
+
+impl From<TrafficLight<Green>> for TrafficLight<Yellow> {
+    fn from(green: TrafficLight<Green>) -> TrafficLight<Yellow> {
+        println!("last state is {:?}", green);
+        TrafficLight {
+            state: Yellow::new(),
+        }
+    }
+}
+
+impl From<TrafficLight<Yellow>> for TrafficLight<Red> {
+    fn from(yellow: TrafficLight<Yellow>) -> TrafficLight<Red> {
+        println!("last state is {:?}", yellow);
+        TrafficLight {
+            state: Red::new(),
+        }
+    }
+}
+
+impl From<TrafficLight<Red>> for TrafficLight<Green> {
+    fn from(red: TrafficLight<Red>) -> TrafficLight<Green> {
+        println!("last state is {:?}", red);
+        TrafficLight {
+            state: Green::new(),
+        }
+    }
+}
+
+enum TrafficLightWrapper {
+    Red(TrafficLight<Red>),
+    Green(TrafficLight<Green>),
+    Yellow(TrafficLight<Yellow>),
+}
+
+impl TrafficLightWrapper {
+    fn step(mut self) -> Self {
+        match self {
+            TrafficLightWrapper::Green(green) => TrafficLightWrapper::Yellow(green.into()),
+            TrafficLightWrapper::Yellow(yellow) => TrafficLightWrapper::Red(yellow.into()),
+            TrafficLightWrapper::Red(red) => TrafficLightWrapper::Green(red.into())
+        }
+    }
+}
+
+struct Factory {
+    traffic_light: TrafficLightWrapper
+}
+
+impl Factory {
+    fn new() -> Self {
+        Factory{
+            traffic_light: TrafficLightWrapper::Green(TrafficLight::new())
         }
     }
 }
 
 fn main() {
-    let mut state_machine = TrafficLight::new();
+    let mut state_machine = Factory::new();
 
     loop {
-        println!("{:?}", state_machine.state);
-        state_machine.change_light();
+        state_machine.traffic_light = state_machine.traffic_light.step();
     }
 }
